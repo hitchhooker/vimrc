9265352eed9d3ce35838738c7a06091e46550a2e
hitchhooker <tommi@niemi.lol>
2025-03-19 07:35:19 +0700

    default to # commentstring

--- config/options.lua
+++ config/options.lua
@@ -3,6 +3,11 @@
 -- Add any additional options here
 vim.opt.conceallevel = 1
 
+-- Ensure commentstring is set
+if vim.bo.commentstring == "" or vim.bo.commentstring == nil then
+  vim.bo.commentstring = "# %s"
+end
+
 return {
   {
     "folke/which-key.nvim",
@@ -15,23 +20,6 @@ return {
       -- your configuration comes here
       -- or leave it empty to use the default settings
       -- refer to the configuration section below
-      -- ChatGPT keymaps
-      g = {
-        name = "ChatGPT",
-        c = { "<cmd>ChatGPT<CR>", "ChatGPT" },
-        e = { "<cmd>ChatGPTEditWithInstruction<CR>", "Edit with instruction", mode = { "n", "v" } },
-        g = { "<cmd>ChatGPTRun grammar_correction<CR>", "Grammar Correction", mode = { "n", "v" } },
-        t = { "<cmd>ChatGPTRun translate<CR>", "Translate", mode = { "n", "v" } },
-        k = { "<cmd>ChatGPTRun keywords<CR>", "Keywords", mode = { "n", "v" } },
-        d = { "<cmd>ChatGPTRun docstring<CR>", "Docstring", mode = { "n", "v" } },
-        a = { "<cmd>ChatGPTRun add_tests<CR>", "Add Tests", mode = { "n", "v" } },
-        o = { "<cmd>ChatGPTRun optimize_code<CR>", "Optimize Code", mode = { "n", "v" } },
-        s = { "<cmd>ChatGPTRun summarize<CR>", "Summarize", mode = { "n", "v" } },
-        f = { "<cmd>ChatGPTRun fix_bugs<CR>", "Fix Bugs", mode = { "n", "v" } },
-        x = { "<cmd>ChatGPTRun explain_code<CR>", "Explain Code", mode = { "n", "v" } },
-        r = { "<cmd>ChatGPTRun roxygen_edit<CR>", "Roxygen Edit", mode = { "n", "v" } },
-        l = { "<cmd>ChatGPTRun code_readability_analysis<CR>", "Code Readability Analysis", mode = { "n", "v" } },
-      },
     },
   },
 }
